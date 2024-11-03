fe12ff5910e0184e0aa340e76bfc5b7cda7da4cd
Ethan <zhangethan823@gmail.com>
2024-11-02 19:37:42 -0700

    Update servo-motor-arduino.ino

--- servo-motor-arduino/servo-motor-arduino.ino
+++ servo-motor-arduino/servo-motor-arduino.ino
@@ -1,55 +1,41 @@
 #include <Servo.h>
 
-const int buttonPin = 2;
-Servo nine;
+const int button1Pin = 2;
+const int servo1Pin = 9;
 const int motor1in1 = 10;
 const int motor1in2 = 11;
 
-int buttonState =  0;
-//int a = 0;
+Servo servo1;
+
+int button1State = 0;
+bool button1Pressed = false;
 
 void setup() {
   // put your setup code here, to run once:
 
-  nine.attach(9);  // attaches the servo on pin 9 to the servo object
-  pinMode(buttonPin, INPUT);
+  servo1.attach(servo1Pin);  // attaches the servo on pin 9 to the servo object
+  servo1.write(180);
+  pinMode(button1Pin, INPUT);
   pinMode(motor1in1, OUTPUT);
   pinMode(motor1in2, OUTPUT);
-  nine.write(0);
-
 }
 
 void loop() {
-
-/*
-  for (a = 0; a < 180; a+=5)
-  {
-    analogWrite(motor1in2, 50);
-    nine.write(a);
-    delay(15);
-  }
-  delay(1000);
-  for (a = 180; a > 0; a-=5)
-  {
-    analogWrite(motor1in2, 0);
-
-    nine.write(a);
-    delay(15);
-  }
-  delay(1000);
-}
-  */
   // put your main code here, to run repeatedly:
-  buttonState = digitalRead(buttonPin);
+  button1State = digitalRead(button1Pin);
 
-  digitalWrite(motor1in2, LOW);
-  if (buttonState == HIGH) {
-    //button1Pressed = true;
-    analogWrite(motor1in2, 50);
+  digitalWrite(motor1in1, LOW);
+  if(button1State == HIGH && button1Pressed==false){
+    button1Pressed = true;
+    servo1.write(0);
+    analogWrite(motor1in2, 70);
     delay(1000);
-    nine.write(180);
+    analogWrite(motor1in2, 0);
+    servo1.write(180);
     delay(500);
-    nine.write(0);
+    servo1.write(0);
   }
-  
+  else if(button1State == LOW && button1Pressed==true){ //debounce
+    button1Pressed = false;
+  }
 }
